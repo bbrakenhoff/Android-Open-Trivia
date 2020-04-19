eb961cfcb9cbd34c8c9cb6f1742e0fe260a9d513
Bijoya Brakenhoff <>
2020-04-19 15:13:26 +0200

    Downgrade gradle in order to use MockK in instrumented tests

--- app/src/androidTest/java/com/bbrakenhoff/opentrivia/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/bbrakenhoff/opentrivia/ExampleInstrumentedTest.kt
@@ -1,19 +1,18 @@
 package com.bbrakenhoff.opentrivia
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.bbrakenhoff.opentrivia.model.TriviaCategory
+import com.google.common.truth.Truth.assertThat
+import io.mockk.mockk
+import junit.framework.Assert.assertEquals
 
 import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
